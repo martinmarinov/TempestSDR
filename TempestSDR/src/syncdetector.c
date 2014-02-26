b2b0470758a576ec6f962abf0b3ef2abb8034894
Martin Marinov <martintzvetomirov@abv.com>
2014-02-26 21:12:28 +0000

    Still fail but roughness calculation is in a function on its own

--- TempestSDR/src/syncdetector.c
+++ TempestSDR/src/syncdetector.c
@@ -78,46 +78,104 @@ void gaussianblur(float * data, int size) {
 	}
 }
 
-int findthesweetspot(float * data, int size, float varq) {
+
+
+// calculate the flattness of a strip in circular data
+// sum is the sum of all of the elements in the strip
+// offset is where the strip starts
+// stripsize is the size of the strip
+// return value is >=0. The bigger it is the less flat the curve is
+float calculateroughness(float * data, int size, int offset, int stripsize, float sum) {
+	// feedback loop, if we were growing and this is giving worse results, stop growing or the other way around
+		int i;
+		const int stripend = offset+stripsize;
+		int c = 0;
+		const int halfstripsize = stripsize >> 1;
+		const double quorterstripsizef = stripsize / 4.0;
+		float trianglecenteredsum = 0.0;
+		for (i = offset; i < stripend; i++) {
+			const int realid = (i < size) ? (i) : (i - size); // i % size
+			const double coeff = ((c < halfstripsize) ? (c/quorterstripsizef) : (4.0-c/quorterstripsizef));
+			trianglecenteredsum += coeff * data[realid];
+			c++;
+		}
+
+		// difference between bestfitsum and triangled sum
+		const double dbestfitcurrent = (sum - trianglecenteredsum) / (float) stripsize;
+
+		// the bigger the value is, the less flat the data is
+		return dbestfitcurrent * dbestfitcurrent;
+}
+
+#define SWEETSPOT_INIT {.growsize=0, .stripsize_val=0, .bestfit = 0.0f}
+typedef struct sweetspot_data {
+	int growsize;
+	int stripsize_val;
+
+	double bestfit;
+} sweetspot_data_t;
+
+int findthesweetspot(sweetspot_data_t * db, float * data, int size, int minsize) {
 	int i;
+	const int size2 = size >> 1;
+
+	db->stripsize_val+=db->growsize;
+
+	if (db->stripsize_val > size2) db->stripsize_val = size2;
+	if (db->stripsize_val < minsize) db->stripsize_val = minsize;
+
+	const int stripsize = db->stripsize_val;
+	const double stripsizef = stripsize;
+	const double bigstripsizef = size - stripsize;
 
 	gaussianblur(data, size);
 
-	// the size of the small strip that will be looked for
-	const int stripsize = varq * size;
-	const float stripsizef = stripsize;
-	const float bigstripsizef = size - stripsizef;
-
-	float currsum = 0.0f;
+	double currsum = 0.0;
 	for (i = 0; i < stripsize; i++) currsum += data[i];
 
-	float totalsum = currsum;
+	double totalsum = currsum;
 	for (i = stripsize; i < size; i++) totalsum += data[i];
 
 	// totalsum - currsum is the sum in the remainder of the strip
 	// we want to maximize the difference squared
-	const float bestfitsqzero = (totalsum - currsum)/bigstripsizef - currsum/stripsizef;
-	float bestfit = bestfitsqzero * bestfitsqzero;
+	const double bestfitsqzero = (totalsum - currsum)/bigstripsizef - currsum/stripsizef;
+	double bestfit = bestfitsqzero * bestfitsqzero;
 	int bestfitid = 0;
 
-	for (i = 0; i < size; i++) {
+	const int size1 = size - 1;
+	const int sizemstepsize = size-stripsize;
+	float roughness = INFINITY;
+	for (i = 0; i < size1; i++) {
 		// i is the id to remove from the sum
 		// i + stripsize is the id to add to the sum
-		const float datatoremove = data[i];
-		const float datatoadd = data[(i+stripsize) % size];
+		const double datatoremove = data[i];
+		const int toremoveid = (i < sizemstepsize) ? (i+stripsize) : (i-sizemstepsize); // (i+stripsize) % size
+		const double datatoadd = data[toremoveid];
 
 		currsum = currsum - datatoremove + datatoadd;
-		const float bestfitsq = (totalsum - currsum)/bigstripsizef - currsum/stripsizef;
-		const float bestfitcurr = bestfitsq * bestfitsq;
+		const double bestfitsq = (totalsum - currsum)/bigstripsizef - currsum/stripsizef;
+		const double bestfitcurr = bestfitsq * bestfitsq;
 
 		if (bestfitcurr > bestfit) {
-			bestfit = bestfitcurr;
-			bestfitid = i;
+			const float roughnesscurr = calculateroughness(data, size, i, stripsize, currsum);
+			if (roughnesscurr < roughness) {
+				roughness = roughnesscurr;
+				bestfit = bestfitcurr;
+				bestfitid = i;
+			}
 		}
 	}
 
+	int dirtochange = 0;
+	if (bestfit < db->bestfit) dirtochange++;
+	db->growsize = (db->growsize > 0) ? (-dirtochange) : (dirtochange);
+	db->bestfit = bestfit;
+
+	const double sqrtbestfit = sqrtf(bestfit);
+	const int stripend = bestfitid+stripsize;
+	for (i = bestfitid; i < stripend; i++) data[i % size] = sqrtbestfit;
 	data[bestfitid] = PIXEL_SPECIAL_VALUE_B;
-	data[(bestfitid+stripsize) % size] = PIXEL_SPECIAL_VALUE_B;
+	data[stripend % size] = PIXEL_SPECIAL_VALUE_B;
 
 	return (bestfitid + stripsize/2) % size;
 }
@@ -136,8 +194,21 @@ void horizontalline(int y, float * data, int width, int height, float val) {
 
 float * syncdetector_run(tsdr_lib_t * tsdr, float * data, float * outputdata, int width, int height, float * widthbuffer, float * heightbuffer) {
 
-	const int dxorig = findthesweetspot(widthbuffer, width, 0.25f);
-	const int dyorig = findthesweetspot(heightbuffer, height, 0.05f);
+	static sweetspot_data_t db_x = SWEETSPOT_INIT, db_y = SWEETSPOT_INIT;
+
+	static float olddx = 0.0f, olddy = 0.0f;
+
+	int origdx = findthesweetspot(&db_x, widthbuffer, width, width * 0.1f );
+	int origdy = findthesweetspot(&db_y, heightbuffer, height, height * 0.01f );
+
+	if (origdx - olddx > width / 2) olddx += width; else if (olddx - origdx > width / 2) origdx += width;
+	if (origdy - olddy > height / 2) olddy += height; else if (olddy - origdy > height / 2) origdy += height;
+
+	olddx = ((int) (0.5f*origdx + 0.5f*olddx)) % width;
+	olddy = ((int) (0.5f*origdy + 0.5f*olddy)) % height;
+
+	const int dx = olddx;
+	const int dy = olddy;
 
 	int i;
 	const int size = width * height;
@@ -167,28 +238,28 @@ float * syncdetector_run(tsdr_lib_t * tsdr, float * data, float * outputdata, in
 	// do the shift itself
 	if (tsdr->params_int[PARAM_INT_AUTOSHIFT]) {
 		// fix the y offset
-		const int ypixels = dyorig*width;
+		const int ypixels = dy*width;
 		const int ypixelsrem = size-ypixels;
-		const int xrem = width - dxorig;
+		const int xrem = width - dx;
 		const int xremfloatsize = xrem * sizeof(float);
-		const int dxorigfloatsize = dxorig * sizeof(float);
+		const int dxorigfloatsize = dx * sizeof(float);
 
 		int yshift = 0;
 		for (yshift = 0; yshift < ypixels; yshift+=width) {
 			const int offset = ypixelsrem+yshift;
 			memcpy(&outputdata[offset+xrem], &data[yshift], dxorigfloatsize); // TL to BR
-			memcpy(&outputdata[offset], &data[yshift+dxorig], xremfloatsize); // TR to BL
+			memcpy(&outputdata[offset], &data[yshift+dx], xremfloatsize); // TR to BL
 		}
 		for (yshift = ypixels; yshift < size; yshift+=width) {
 			const int offset = yshift - ypixels;
 			memcpy(&outputdata[offset+xrem], &data[yshift], dxorigfloatsize); // BL to TR
-			memcpy(&outputdata[offset], &data[yshift+dxorig], xremfloatsize); // BR to TL
+			memcpy(&outputdata[offset], &data[yshift+dx], xremfloatsize); // BR to TL
 		}
 
 		return outputdata;
 	} else {
-		verticalline(dxorig, data, width, height, PIXEL_SPECIAL_VALUE_G);
-		horizontalline(dyorig, data, width, height, PIXEL_SPECIAL_VALUE_G);
+		verticalline(dx, data, width, height, PIXEL_SPECIAL_VALUE_G);
+		horizontalline(dy, data, width, height, PIXEL_SPECIAL_VALUE_G);
 		return data;
 	}
 
